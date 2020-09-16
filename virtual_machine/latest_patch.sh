2ca66519ff45e5a0c98b3b4f2eecb1ce3f7fca59
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2020-09-16 12:47:20 +0200

    patch for Noetic geometry2 package

--- virtual_machine/latest_patch.sh
+++ virtual_machine/latest_patch.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-# execute these lines in the console of the Virtual Machine to have it up to date
+# Execute these lines in the console of the Virtual Machine to have it up to date
 
 
-# 7 Sept 2020
+# Downloaded before 16 September 2020
+
+# update ROS 1 path in bashrc
+sed -i 's/"\/opt\/ros\/noetic ~\/ros"/"\/opt\/ros\/noetic \/opt\/local_ws\/ros1 ~\/ros"/' ~/.bashrc
+sed -i 's/"\/opt\/ros\/foxy ~\/ros2"/"\/opt\/ros\/foxy \/opt\/local_ws\/ros2 ~\/ros2"/' ~/.bashrc
+source ~/.bashrc
+ros1ws
+cd /opt/local_ws
+sudo chown ecn . -R
+mkdir -p ros1/src && cd ros1/src
+git clone https://github.com/ros/geometry2.git
+cd ..
+catkin config --extend /opt/ros/$ROS1_DISTRO --install --cmake-args -DCMAKE_BUILD_TYPE=Release
+catkin build
+
 sudo apt update
 sudo apt install -qy texlive-latex-extra texlive-fonts-recommended dvipng
 cd /opt/src
