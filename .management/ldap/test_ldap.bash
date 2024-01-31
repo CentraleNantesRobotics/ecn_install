ada7f373ab83464eee32c3318b8b1880af78f1c4
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-01-31 17:14:34 +0100

    no management tools in this repo

--- .management/ldap/test_ldap.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ldapsearch -x -H ldaps://rldap.ec-nantes.fr -D "okermorg@ec-nantes.fr" -W -b "dc=rldap,dc=ec-nantes,dc=fr" cn
--- .management/vm_update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i "/^export PS1=/c\export PS1=\"\\\[\\\e[1;34m\\\]\\\w\\\[\\\e[0m\\\]$ \"" $1/.bashrc
