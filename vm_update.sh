146e1de555dde62b581c50c396dc2721a5ec9770
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-01-31 10:04:31 +0100

    without s

--- vm_update.sh
+++ vm_update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i 's@export PS1=/c@export PS1=\"\\\[\\\e[1;34m\\\]\\\w\\\[\\\e[0m\\\]$ \"@' ~/.bashrc
+sed -i "/^export PS1=/c\export PS1=\"\\\[\\\e[1;34m\\\]\\\w\\\[\\\e[0m\\\]$ \"" ~/.bashrc
