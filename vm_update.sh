3b2118bf56d4af8e7592790a085fa51bcaf25e84
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-02-10 13:53:16 +0100

    foxy -> galactic

--- vm_update.sh
+++ vm_update.sh
@@ -4,6 +4,7 @@ sed -i "/^export PS1=/c\export PS1=\"\\\[\\\e[1;34m\\\]\\\w\\\[\\\e[0m\\\]$ \"" 
 
 # change to Galactic to enable Ignition Fortress bridge
 if [ ! $(grep -c foxy ~/.bashrc) -eq 0 ]; then
+    echo "Changing ROS 2 from foxy to galactic"
     rm -rf ~/ros2/build ~/ros2/install
     rm -rf /opt/ecn/ros2/build /opt/ecn/ros2/install /opt/ecn/ros2/log
     sed -i 's/foxy/galactic/g' ~/.bashrc
