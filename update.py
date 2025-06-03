15818df932da386374ec6960688e16ed28713f36
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2025-06-03 15:19:12 +0200

    print git fetch

--- update.py
+++ update.py
@@ -455,7 +455,8 @@ class Depend:
             return Status.ABSENT
 
         # check GIT wrt upstream
-        run('git fetch',cwd=base_dir, show=True)
+        print('Fetching',base_dir)
+        run('git fetch',cwd=base_dir)
         git_status = run('git status', cwd=base_dir)
         if git_status is None:
             return Status.OLD
