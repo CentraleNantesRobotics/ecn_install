5b46fb9f68a47380814df3610be96cd90a86aeb9
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2026-04-25 17:55:09 +0200

    copy skel to /etc/skel

--- install_deps.py
+++ install_deps.py
@@ -873,6 +873,7 @@ def perform_update(action = None, poweroff=False):
     with open(bashrc) as f:
         if 'ros_management_tools' not in f.read():
             copytree(skel + '/', os.environ['HOME'], dirs_exist_ok = True)
+            sudo.run(f'rsync -avr {skel}/ /etc/skel/')
 
     sudo.run(f'mkdir -p {Depend.folders[Source.GIT]}')
 
