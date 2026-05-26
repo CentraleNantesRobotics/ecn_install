8ebb2f0625339276fb9e4cc9c56bfe2bcfa26756
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2026-05-26 16:41:08 +0200

    visp without coin3d

--- install_deps.py
+++ install_deps.py
@@ -888,7 +888,8 @@ def perform_update(action = None, poweroff=False):
             copytree(skel + '/', os.environ['HOME'], dirs_exist_ok = True)
             sudo.run(f'rsync -avr {skel}/ /etc/skel/')
 
-    sudo.run(f'mkdir -p {Depend.folders[Source.GIT]}')
+    if not os.path.exists(Depend.folders[Source.GIT]):
+        sudo.run(f'mkdir -p {Depend.folders[Source.GIT]}')
 
     # wallpaper
     wp = [f for f in os.listdir(base_path + '/images/') if distro in f][0]
